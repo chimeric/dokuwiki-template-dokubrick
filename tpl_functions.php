2d6e00e4b99e674e5cf6f91664411f247974dd7c
Michael Klier <chi@chimeric.de>
2007-05-26 16:50:50 +0200

    fixed small bug with toc sidebar
    darcs-hash:20070526145050-23886-933927475b39f5d8c7f6b432258124453fff7cb4.gz

--- tpl_functions.php
+++ tpl_functions.php
@@ -136,10 +136,13 @@ function tpl_sidebar_dispatch($sb) {
                     }
                 }
                 @require_once(DOKU_INC.'inc/parser/xhtml.php');
-                print '<div class="toc_sidebar sidebar_box">' . DOKU_LF;
                 // replace ids to keep XHTML compliance
-                print preg_replace('/id="(.*?)"/', 'id="sb__\1"', Doku_Renderer_xhtml::render_TOC(p_get_metadata($svID,'description tableofcontents')));
-                print '</div>' . DOKU_LF;
+                $toc = preg_replace('/id="(.*?)"/', 'id="sb__\1"', Doku_Renderer_xhtml::render_TOC(p_get_metadata($svID,'description tableofcontents')));
+                if(!empty($toc)) {
+                    print '<div class="toc_sidebar sidebar_box">' . DOKU_LF;
+                    print ($toc);
+                    print '</div>' . DOKU_LF;
+                }
             }
             break;
         
